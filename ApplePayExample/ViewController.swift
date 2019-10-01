8c98ea91e512f5497c4448399feb49a018833fb8
Станислав Шияновский <st-small@rambler.ru>
2019-10-01 14:59:58 +0300

    Initial Commit

---
+++ ApplePayExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePayExample
+//
+//  Created by Станислав Шияновский on 10/1/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
