364bbe1813b51b7c35ac94240925f0c0495098b1
Станислав Шияновский <st-small@rambler.ru>
2019-10-01 19:19:25 +0300

    Created sample project.

--- ApplePayExample/ViewController.swift
+++ ApplePayExample/ViewController.swift
@@ -6,15 +6,43 @@
 //  Copyright © 2019 Станислав Шияновский. All rights reserved.
 //
 
+import PassKit
 import UIKit
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController {
+    
+    private var paymentRequest: PKPaymentRequest = {
+        let request = PKPaymentRequest()
+        request.merchantIdentifier = "merchant.ua.kr.ssh.ApplePayExample"
+        request.supportedNetworks = [.visa, .masterCard]
+        request.supportedCountries = ["US"]
+        request.merchantCapabilities = .capability3DS
+        request.countryCode = "US"
+        request.currencyCode = "USD"
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "iPhone Xs 64 Gb", amount: 34999.99)]
+        return request
+    }()
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func purchase(_ sender: Any?) {
+        if let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) {
+            controller.delegate = self
+            present(controller, animated: true, completion: nil)
+        }
+    }
 }
 
+extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+    
+    public func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+    
+    public func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
